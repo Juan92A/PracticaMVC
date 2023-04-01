8ace648be5019cf475851b56cc5cd7303e54cf0d
Juan92A <acunajuan429@gmail.com>
2023-03-31 22:38:45 -0600

    agregando archivos

---
+++ PracticaMVC/Models/Marcas.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaMVC.Models
+{
+    public class Marcas
+    {
+        [Key]
+        public int id_marcas { get; set; }
+        [Display(Name = "Nombre de Marca")]
+
+        public string? nombre_marca { get; set; }
+        [Display(Name = "Estado")]
+
+        public string? estados { get; set; }
+    }
+}
---
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+       
+            public equiposDbContext(DbContextOptions options) : base(options)
+            {
+
+            }
+
+
+            public DbSet<Marcas> Marcas { get; set; }
+    
+    }
+}
